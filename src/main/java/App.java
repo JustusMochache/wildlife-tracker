a7df228ac7635ba91d5a490478780309fdc15c5e
MoringaSchool <you@example.com>
2020-01-12 15:27:37 +0300

    add get method in app.java and public route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,21 @@
+import models.Animal;
+import models.Sighting;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        Connection conn;
+        staticFileLocation("/public");
+
+        get("/", (req,res)->{
+            Map<String, Object> model = new HashMap<>();
+            model.put("allEndangered", EndangeredAnimal.getAllAnimals());
+            model.put("allAnimals", Animal.getAllAnimals());
+            model.put("allSightings", Sighting.allSightings());
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
 }
